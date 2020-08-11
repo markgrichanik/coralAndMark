2507081dae24186589051290dc329585c403c88c
grichm1 <mark.grichanik@dell.com>
2020-08-11 17:47:18 +0300

    changes5

--- js/scripts.js
+++ js/scripts.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
     });
 
     /********************** Social Share buttons ***********************/
-    // var share_bar = document.getElementsByClassName('share-bar');
+    /** var share_bar = document.getElementsByClassName('share-bar');
     // var po = document.createElement('script');
     // po.type = 'text/javascript';
     // po.async = true;
@@ -160,7 +160,7 @@ $(document).ready(function () {
     //     share_bar[i].innerHTML = html;
     //     share_bar[i].style.display = 'inline-block';
     // }
-
+    **/
     /********************** Embed youtube video *********************/
     $('.player').YTPlayer();
 
